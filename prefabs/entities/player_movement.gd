6d3c6bb3cc9d30a16d97efdb4628b8c1a7e4097c
Bennett Smrdel <fireknights318@gmail.com>
2024-05-04 11:44:45 -0500

    feat: Add player movement

---
+++ prefabs/entities/player_movement.gd
@@ -0,0 +1,22 @@
+extends CharacterBody2D
+
+
+@export var speed = 300.0
+@export var jump_velocity = -400.0
+@export var gravity = 980
+
+
+func _physics_process(delta):
+	if not is_on_floor():
+		velocity.y += gravity * delta
+
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = jump_velocity
+
+	var direction = Input.get_axis("move_left", "move_right")
+	if direction:
+		velocity.x = direction * speed
+	else:
+		velocity.x = move_toward(velocity.x, 0, speed)
+
+	move_and_slide()
